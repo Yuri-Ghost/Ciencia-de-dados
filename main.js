fd58743c26f438c84d5398e30bf4f4a7974e04c4
Yuri Augusto (Yandi) <126188473+Yuri-Ghost@users.noreply.github.com>
2024-10-02 09:34:37 -0300

    oxi samir

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
-const url = "https://servicodados.ibge.gov.br/api/v3/agregados/1969/periodos/2000/variaveis/93%7C1000093?localidades=N1[all]&classificacao=1[0]%7C133[all]
+const url = "https://servicodados.ibge.gov.br/api/v3/agregados/1969/periodos/2000/variaveis/93%7C1000093?localidades=N1[all]&classificacao=1[0]%7C133[all]"
 
-async function visualizarDados(){const res = await fetch(url)}
+async function visualizarDados(){const res = await fetch(url)
 
 const dados = await res.json()
-console.log (dados)
+
+const tipo_dados = dados[0]['variavel']
+console.log(tipo_dados)
+const regiao = dados[0]["resultados"]
+console.log(regiao)
 }
 
 visualizarDados()
