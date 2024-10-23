13ef79e05977c5f68ae8a6c1b36019bd6cd480bc
Yuri Augusto (Yandi) <126188473+Yuri-Ghost@users.noreply.github.com>
2024-10-23 09:28:38 -0300

    amogus

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const url = "https://servicodados.ibge.gov.br/api/v3/agregados/1969/periodos/2000/variaveis/93%7C1000093?localidades=N1[all]&classificacao=1[0]%7C133[all]"
 
-async function visualizarDados(){const res = await fetch(url)
-
+async function visualizarDados(){
+const res = await fetch(url)
 const dados = await res.json()
-
+regiao = 0
 const tipo_dados = dados[0]['variavel']
 console.log(tipo_dados)
 const regiao = dados[0]["resultados"]
